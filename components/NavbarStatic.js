65ddcb1689a984e33e9283a75a009e8831fcaa02
sourav-okobiz <juniordev.okobiz@gmail.com>
2024-11-15 04:25:20 +0600

    done

--- components/NavbarStatic.js
+++ components/NavbarStatic.js
@@ -1,3 +1,4 @@
+"use client"
 import React, { useEffect, useState } from 'react';
 import { BriefcaseBusiness, GripVertical, Home, Mail, Shapes, StretchHorizontal, TextSelect, User } from "lucide-react";
 import Link from 'next/link';
