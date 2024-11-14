6e53209cf1080468293d8dcc29b0b69c29408040
sourav-okobiz <juniordev.okobiz@gmail.com>
2024-11-15 04:21:27 +0600

    fix contact

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,29 +1,40 @@
-import { Mail, Shapes } from 'lucide-react';
-import Header from './Header';
+import { Mail, Shapes } from "lucide-react";
+import Header from "./Header";
 
 const ContactForm = () => {
   return (
-    <div >
+    <div>
       <div className="custom-container">
         <div className="contact-content content-width max-w-4xl mx-auto">
           <div className="section-header mb-10">
-          <div className="flex mb-16">
+            <div className="flex mb-16">
               <Header title="Contact" HomeIcon={Mail} />
             </div>
 
             <h1 className="text-white text-4xl leading-tight font-light mb-8">
-            Let's Work  <span className="text-[#FFCC01]">Together!</span>
+              Let&apos;s Work <span className="text-[#FFCC01]">Together!</span>
             </h1>
           </div>
-          <h3 className="text-lg font-medium text-gray-300 mb-4">souravacharjee360@gmail.com</h3>
+          <h3 className="text-lg font-medium text-gray-300 mb-4">
+            souravacharjee360@gmail.com
+          </h3>
           <p id="required-msg" className="text-sm  text-gray-500 mb-8">
             * Marked fields are required to fill.
           </p>
 
-          <form className="contact-form space-y-6" method="POST" action="mailer.php">
+          <form
+            className="contact-form space-y-6"
+            method="POST"
+            action="mailer.php"
+          >
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
               <div className="input-group">
-                <label htmlFor="full-name" className="text-sm font-medium text-gray-300 mb-2">Full Name <sup>*</sup></label>
+                <label
+                  htmlFor="full-name"
+                  className="text-sm font-medium text-gray-300 mb-2"
+                >
+                  Full Name <sup>*</sup>
+                </label>
                 <input
                   type="text"
                   name="full-name"
@@ -33,7 +44,12 @@ const ContactForm = () => {
                 />
               </div>
               <div className="input-group">
-                <label htmlFor="email" className="text-sm font-medium text-gray-300 mb-2">Email <sup>*</sup></label>
+                <label
+                  htmlFor="email"
+                  className="text-sm font-medium text-gray-300 mb-2"
+                >
+                  Email <sup>*</sup>
+                </label>
                 <input
                   type="email"
                   name="email"
@@ -43,7 +59,12 @@ const ContactForm = () => {
                 />
               </div>
               <div className="input-group">
-                <label htmlFor="phone-number" className="text-sm font-medium text-gray-300 mb-2">Phone <span className="text-gray-500">(optional)</span></label>
+                <label
+                  htmlFor="phone-number"
+                  className="text-sm font-medium text-gray-300 mb-2"
+                >
+                  Phone <span className="text-gray-500">(optional)</span>
+                </label>
                 <input
                   type="text"
                   name="phone-number"
@@ -53,22 +74,59 @@ const ContactForm = () => {
                 />
               </div>
               <div className="input-group">
-                <label htmlFor="subject" className="text-sm font-medium text-gray-300 mb-2">Subject <sup>*</sup></label>
+                <label
+                  htmlFor="subject"
+                  className="text-sm font-medium text-gray-300 mb-2"
+                >
+                  Subject <sup>*</sup>
+                </label>
                 <select
                   name="subject"
                   id="subject"
                   className="input-field w-full px-4 py-3 border border-gray-700 bg-transparent text-white rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary transition duration-300"
                 >
-                  <option className='bg-gray-800 cursor-pointer' value="">Select a subject</option>
-                  <option className='bg-gray-800 cursor-pointer' value="subject1">UI Design</option>
-                  <option className='bg-gray-800 cursor-pointer'  value="subject2">Frontend Development</option>
-                  <option className='bg-gray-800 cursor-pointer' value="subject3">Full Stack Development</option>
-                  <option className='bg-gray-800 cursor-pointer' value="subject3">Backend Development</option>
-                  <option className='bg-gray-800 cursor-pointer' value="subject3">App Development</option>
+                  <option className="bg-gray-800 cursor-pointer" value="">
+                    Select a subject
+                  </option>
+                  <option
+                    className="bg-gray-800 cursor-pointer"
+                    value="subject1"
+                  >
+                    UI Design
+                  </option>
+                  <option
+                    className="bg-gray-800 cursor-pointer"
+                    value="subject2"
+                  >
+                    Frontend Development
+                  </option>
+                  <option
+                    className="bg-gray-800 cursor-pointer"
+                    value="subject3"
+                  >
+                    Full Stack Development
+                  </option>
+                  <option
+                    className="bg-gray-800 cursor-pointer"
+                    value="subject3"
+                  >
+                    Backend Development
+                  </option>
+                  <option
+                    className="bg-gray-800 cursor-pointer"
+                    value="subject3"
+                  >
+                    App Development
+                  </option>
                 </select>
               </div>
               <div className="input-group">
-                <label htmlFor="budget" className="text-sm font-medium text-gray-300 mb-2">Your Budget <span className="text-gray-500">(optional)</span></label>
+                <label
+                  htmlFor="budget"
+                  className="text-sm font-medium text-gray-300 mb-2"
+                >
+                  Your Budget <span className="text-gray-500">(optional)</span>
+                </label>
                 <input
                   type="number"
                   name="budget"
@@ -78,7 +136,12 @@ const ContactForm = () => {
                 />
               </div>
               <div className="input-group">
-                <label htmlFor="message" className="text-sm font-medium text-gray-300 mb-2">Message</label>
+                <label
+                  htmlFor="message"
+                  className="text-sm font-medium text-gray-300 mb-2"
+                >
+                  Message
+                </label>
                 <textarea
                   name="message"
                   id="message"
@@ -87,7 +150,10 @@ const ContactForm = () => {
                 ></textarea>
               </div>
               <div className="input-group">
-                <label htmlFor="upload-attachment" className="flex items-center space-x-2 text-sm font-medium text-gray-300 mb-2">
+                <label
+                  htmlFor="upload-attachment"
+                  className="flex items-center space-x-2 text-sm font-medium text-gray-300 mb-2"
+                >
                   <Shapes className="w-5 h-5 text-primary" />
                   <span>Add an attachment</span>
                 </label>
